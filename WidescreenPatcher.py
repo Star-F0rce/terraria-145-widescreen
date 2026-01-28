c489edba7a1c99c749192432a8dc79b67a8d06f7
Star-F0rce <94090042+Star-F0rce@users.noreply.github.com>
2026-01-28 11:46:31 -0500

    Update to 1.4.5.1

--- WidescreenPatcher.py
+++ WidescreenPatcher.py
@@ -1,14 +1,16 @@
 import argparse
 
 
-parser = argparse.ArgumentParser(description="Widescreen Patcher for Terraria")
+parser = argparse.ArgumentParser(description="Widescreen Patcher for Terraria 1.4.5.1")
 
 parser.add_argument("file", type=str, help="Terraria.exe path")
 parser.add_argument("width", type=int, help="Monitor width")
 parser.add_argument("height", type=int, help="Monitor Height")
 args = parser.parse_args()
-offset_width = 4212051
-offset_height = 4212056
+offset_width_loc = "0x4049D3"
+offset_height_loc = "0x4049D8"
+offset_width = int(offset_width_loc,16)
+offset_height = int("0x4049D3",16)
 width_value = args.width - 1
 height_value = args.height - 1
 
@@ -17,13 +19,13 @@ height_hex = height_value.to_bytes(2, byteorder="little", signed=False)
 
 if args.width > 1920:
     with open(args.file, "r+b") as f:
-        print(f"Writing {width_hex.hex()} at 0x404553")
+        print(f"Writing {width_hex.hex()} at {offset_width_loc}")
         f.seek(offset_width)
         f.write(width_hex)
     
 if args.height > 1200:
     with open(args.file, "r+b") as f:
-        print(f"Writing {height_hex.hex()} at 0x404558")
+        print(f"Writing {height_hex.hex()} at {offset_height_loc}")
         f.seek(offset_height)
         f.write(height_hex)
 
