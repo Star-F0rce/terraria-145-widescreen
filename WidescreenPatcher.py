8c030ff813c4b9a14bb5d121887fbf41b66e57e2
Star-F0rce <94090042+Star-F0rce@users.noreply.github.com>
2026-01-28 14:11:26 -0500

    Updated 1.4.5.x

--- WidescreenPatcher.py
+++ WidescreenPatcher.py
@@ -7,27 +7,33 @@ parser.add_argument("file", type=str, help="Terraria.exe path")
 parser.add_argument("width", type=int, help="Monitor width")
 parser.add_argument("height", type=int, help="Monitor Height")
 args = parser.parse_args()
-offset_width_loc = "0x4049D3"
-offset_height_loc = "0x4049D8"
-offset_width = int(offset_width_loc,16)
-offset_height = int(offset_height_loc,16)
 width_value = args.width - 1
 height_value = args.height - 1
 
+
+MaxWorldViewSize = bytes.fromhex("04 20 80 07 00 00 20 B0 04")
+
+with open(args.file, "rb") as f:
+    data = bytearray(f.read())
+
+index = data.find(MaxWorldViewSize)
+
+if index == -1:
+    raise RuntimeError("Pattern not found")
+else:
+    print(f"MaxWorldViewSize found")
+
 width_hex = width_value.to_bytes(2, byteorder="little", signed=False)
 height_hex = height_value.to_bytes(2, byteorder="little", signed=False)
-
 if args.width > 1920:
-    with open(args.file, "r+b") as f:
-        print(f"Writing {width_hex.hex()} at {offset_width_loc}")
-        f.seek(offset_width)
-        f.write(width_hex)
-    
+    print(f"Replacing width with {width_hex.hex()}")
+    data[index + 2 : index + 4] = width_hex
+
 if args.height > 1200:
-    with open(args.file, "r+b") as f:
-        print(f"Writing {height_hex.hex()} at {offset_height_loc}")
-        f.seek(offset_height)
-        f.write(height_hex)
+    print(f"Replacing height with {height_hex.hex()}")
+    data[index + 7 : index + 9] = height_hex
 
-print(f"All done! Have fun with 1.4.5!")
+with open(args.file, "wb") as f:
+    f.write(data)
 
+print("All done! Have fun with 1.4.5!")
